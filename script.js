9d1cd3ede3a301621b56617e523f17319576fe3e
matt485 <113977874+Matt485@users.noreply.github.com>
2022-10-25 10:14:14 +0200

    Skeleton structure

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const typePara = document.createElement("p");
+
+const typeWrite = (string, element) => {
+    let intervalID;
+    for (let i = 0, len = string.length; i < len; i++) {
+        setInterval();
+    };
+    stopInterval();
+};
+
+document.body.appendChild(typePara);
+
+typeWrite("Prout", typePara);
